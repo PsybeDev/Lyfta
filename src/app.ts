247a4aec94c26f37ba4754d05e876b03fce48413
JB Wood <jb.wood@windsorcircle.com>
2019-01-25 21:19:53 -0600

    create inital route and controller

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import path from 'path';
 
+// import controllers
+import * as index from "./controllers/index";
+
 const app = express();
 
 // espress configuration
 app.set('port', process.env.port || 3000);
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
+
+
+// app routes
+app.get('/', index.index);
 
 export default app;
