30260df1e21be8fb57258370a9ce1e7b34c56a16
JB Wood <jb.wood@windsorcircle.com>
2019-08-17 21:39:42 -0500

    plugup mongodb, add eslint and .env

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import path from 'path';
-// import session from 'express-session';
-// import mongo from 'connect-mongo';
-// import mongoose from 'mongoose';
+import session from 'express-session';
+import mongo from 'connect-mongo';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-// import bluebird from 'bluebird';
-// import { MONGODB_URI, SESSION_SECRET } from "./utils/secrete";
+import bluebird from 'bluebird';
+import { MONGODB_URI, SESSION_SECRET } from './utils/secrete';
 // for graphQL
 import { ApolloServer } from 'apollo-server-express';
 import depthLimit from 'graphql-depth-limit';
@@ -13,13 +13,13 @@ import compression from 'compression';
 import cors from 'cors';
 import schema from './schema';
 
-// const MongoStore = mongo(session);
+const MongoStore = mongo(session);
 
 // load environment variables
 dotenv.config({path: '.env.example'})
 
 // import controllers
-import * as index from "./controllers/index";
+import * as index from './controllers/index';
 
 // create server
 const app = express();
@@ -30,14 +30,15 @@ const graphQLServer = new ApolloServer({
 });
 
 // connect to mongo
-// const mongoUrl = MONGODB_URI;
-// (<any>mongoose).Promise = bluebird;
-// mongoose.connect(mongoUrl, {useMongoClient: true}).then(
-// 	() => {}
-// ).catch(err => {
-// 	console.log('MongoDB connection error. Make sure MongoDB is runnning.' + err);
-// 	// process.exit();
-// });
+const mongoUrl = MONGODB_URI;
+mongoose.Promise = bluebird;
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true }).then(() => {
+	console.log('MongoDB Connected!');
+}).catch(err => {
+	console.log('MongoDB connection error. Make sure MongoDB is runnning.' + err);
+	process.exit();
+});
 
 // express configuration
 app.set('port', process.env.port || 3000);
@@ -45,15 +46,15 @@ app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 app.use('*', cors());
 app.use(compression());
-// app.use(session({
-// 	resave: true,
-// 	saveUninitialized: true,
-// 	secret: SESSION_SECRET,
-//   store: new MongoStore({
-//     url: mongoUrl,
-//     autoReconnect: true
-//   })
-// }));
+app.use(session({
+  resave: true,
+	saveUninitialized: true,
+	secret: SESSION_SECRET,
+  store: new MongoStore({
+    url: mongoUrl,
+    autoReconnect: true
+  })
+}));
 
 // path to graphQL server
 graphQLServer.applyMiddleware({app, path: '/graphql'});
--- src/utils/secrete.ts
+++ src/utils/secrete.ts
@@ -1,6 +1,30 @@
-const MONGODB_URI = '';
-const SESSION_SECRET = '';
-export {
-	MONGODB_URI,
-	SESSION_SECRET
-};
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+if (fs.existsSync('.env')) {
+	console.log('Using .env file to supply config environment variables');
+	dotenv.config({ path: '.env' });
+} else {
+	console.log('Using .env.example file to supply config environment variables');
+	dotenv.config({ path: '.env.example' });  // you can delete this after you create your own .env file!
+}
+
+export const ENVIRONMENT = process.env.NODE_ENV;
+const prod = ENVIRONMENT === 'production';
+
+export const SESSION_SECRET = process.env['SESSION_SECRET'];
+export const MONGODB_URI = process.env['MONGODB_URI'];
+
+if (!SESSION_SECRET) {
+	console.error('No client secret. Set SESSION_SECRET environment variable.')
+	process.exit(1);
+}
+
+if (!MONGODB_URI) {
+	if (prod) {
+		console.error('No mongo connection string. Set MONGODB_URI environment variable.');
+	} else {
+		console.error('No mongo connection string. Set MONGODB_URI_LOCAL environment variable.');
+	}
+	process.exit(1);
+}
